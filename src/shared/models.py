485cbd8a7aa851b9aba3cbe26e6c743f1b0b2364
Ceowyllian <downwell@online.ua>
2025-09-19 12:57:05 +0300

    feat(shared): add shared models

---
+++ src/shared/models.py
@@ -0,0 +1,16 @@
+import uuid
+
+from django.contrib.auth import get_user_model
+from model_utils.fields import UUIDField
+from model_utils.models import TimeStampedModel
+
+__all__ = ["BaseModel"]
+
+User = get_user_model()
+
+
+class BaseModel(TimeStampedModel):
+    id = UUIDField(default=uuid.uuid4)
+
+    class Meta:
+        abstract = True
